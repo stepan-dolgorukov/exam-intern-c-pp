bd8d6f504e5630829dff1f194cdb2d1c43eced8b
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-04-04 22:31:04 +0500

    Объявление функции, решающей квадратное уравнение

---
+++ quadratic_equation.h
@@ -0,0 +1,13 @@
+#ifndef QUADRATIC_EQUATION_H
+#define QUADRATIC_EQUATION_H
+
+#include <math.h>
+
+#include "pair_float.h"
+
+struct pair_float
+solve_equation( float a,
+                float b,
+                float c  );
+
+#endif
