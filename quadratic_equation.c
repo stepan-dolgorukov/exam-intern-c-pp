586e5683153009d57ab7c9e0f65a782f425dfea4
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-04-04 22:31:49 +0500

    Реализация функции solve_equation

---
+++ quadratic_equation.c
@@ -0,0 +1,36 @@
+#include "quadratic_equation.h"
+
+struct pair_float
+solve_equation( float a,
+                float b,
+                float c  )
+{
+  struct pair_float
+    answer = {
+               .components[ 0 ] = NAN,
+               .components[ 1 ] = NAN  };
+
+  if( .0f == a )
+  {
+    return answer;
+  }
+
+  const float
+    discriminant = ( b * b ) - ( 4 * a * c );
+
+  if( .0f > discriminant )
+  {
+    return answer;
+  }
+
+  if( .0f == discriminant )
+  {
+    answer.components[ 0 ] = answer.components[ 1 ] = -b / ( 2.f * a );
+    return answer;
+  }
+
+  answer.components[ 0 ] = ( -b + sqrt( discriminant ) ) / ( 2. * a );
+  answer.components[ 1 ] = ( -b - sqrt( discriminant ) ) / ( 2. * a );
+
+  return answer;
+}
