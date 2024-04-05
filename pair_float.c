6be07ca333d1aeb28b5196e4ec470d85cb641c71
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-04-05 19:33:54 +0500

    Определения функций «in_pair», «in_pair_approximately»

---
+++ pair_float.c
@@ -0,0 +1,59 @@
+#include <stdbool.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <math.h>
+
+#include "pair_float.h"
+
+bool
+in_pair( float value,
+         const struct pair_float* pair )
+{
+  /* Если пара отсутствует, то в ней не может содержаться любой элемент. */
+  if( NULL == pair )
+  {
+    return false;
+  }
+
+  const int length =
+    sizeof( pair->components )
+    /
+    sizeof( pair->components[ 0 ] );
+
+  for( int i = 0; i < length; ++i )
+  {
+    if( value == pair->components[ i ] )
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
+
+bool
+in_pair_approximately( float value,
+                       const struct pair_float* pair,
+                       float precision                )
+{
+  /* Если пара отсутствует, то в ней не может содержаться любой элемент. */
+  if( NULL == pair )
+  {
+    return false;
+  }
+
+  const int length =
+    sizeof( pair->components )
+    /
+    sizeof( pair->components[ 0 ] );
+
+  for( int i = 0; i < length; ++i )
+  {
+    if( fabsf( value - pair->components[ i ] ) < precision )
+    {
+      return true;
+    }
+  }
+
+  return false;
+}
