72e89b33ef907b931ba31205efc731abb2467de9
Stёpa Dolgorukov <stepan.dolgorukov@vk.com>
2024-04-05 06:55:19 +0500

    Объявление функции, находящую принадлежность элемента паре

--- pair_float.h
+++ pair_float.h
@@ -1,9 +1,16 @@
 #ifndef PAIR_FLOAT_H
 #define PAIR_FLOAT_H
 
+#include <stdbool.h>
+
 struct pair_float
 {
   float components[2];
 };
+
+bool
+in_pair( float value,
+         const struct pair_float* pair
+                                       );
 
 #endif
